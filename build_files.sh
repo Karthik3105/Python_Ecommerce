3bde32fc7be2f838dd0d8c1874eadefb8cae12a0
Karthik3105 <83666897+Karthik3105@users.noreply.github.com>
2024-08-24 16:09:17 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -18,8 +18,8 @@ pip_cmd=$(command -v pip || command -v pip3)
 echo "Using pip command: $pip_cmd"
 
 # Install system-level dependencies
-apt-get update
-apt-get install -y libjpeg-dev
+FROM python:3.9-slim
+RUN apt-get update && apt-get install -y libjpeg-dev
 
 
 # Install dependencies
