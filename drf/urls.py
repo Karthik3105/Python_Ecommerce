b59ccc65046ddb3cf35a5e2629f224e50a813dac
Karthik3105 <83666897+Karthik3105@users.noreply.github.com>
2024-08-26 17:47:55 +0530

    Update urls.py

--- drf/urls.py
+++ drf/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('', include('accounts.urls')),
     path('api/password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
-]urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
