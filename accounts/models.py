513ec8fdce813f6bc8577d1ddb3a6008c879b9f4
Karthik3105 <83666897+Karthik3105@users.noreply.github.com>
2024-08-26 17:19:24 +0530

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -7,7 +7,7 @@ from django_rest_passwordreset.signals import reset_password_token_created
 from django.core.mail import send_mail  
 from django.contrib.auth.models import User
 from cloudinary_storage.storage import RawMediaCloudinaryStorage
-
+from cloudinary.models import CloudinaryField
 
 @receiver(reset_password_token_created)
 def password_reset_token_created(sender, instance, reset_password_token, *args, **kwargs):
@@ -40,7 +40,7 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
     product_name = models.CharField(max_length=100)
     product_details = models.TextField()
-    product_image = models.ImageField(upload_to='products/' , blank=True, storage=RawMediaCloudinaryStorage())
+    product_image = models.CloudinaryField(upload_to='products/')
     created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     created_on = models.DateTimeField(auto_now_add=True)
     
