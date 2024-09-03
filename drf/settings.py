bc5b2edbeafe7defda453bc6532505a5115d0554
Karthik3105 <83666897+Karthik3105@users.noreply.github.com>
2024-09-03 21:47:27 +0530

    Update settings.py

--- drf/settings.py
+++ drf/settings.py
@@ -170,7 +170,7 @@ STATICFILES_DIRS = [
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',  # Example for local development
-    'https://react-practice-one-green.vercel.app/',
+    'https://react-practice-one-green.vercel.app',
 ]
 
 
